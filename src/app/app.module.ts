b865ec5dc43d148f68a664951419f365850ff4ef
rakesh.nallakukkala <randyrakesh12@gmail.com>
2022-07-16 17:09:40 +0530

    feat:creating backend functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule} from '@angular/common/http';
 
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
@@ -28,7 +29,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn:'root'
@@ -10,8 +11,14 @@ export class PostService{
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(public http: HttpClient){}
   getPosts(){
-    return [...this.posts];
+    let url = "http://localhost:3000/api/posts";
+    this.http.get<{message: string, posts: Post[]}>(url)
+      .subscribe( (res) => {
+        this.posts = res.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener(){
@@ -19,9 +26,13 @@ export class PostService{
   }
 
   addPost(title: string, content: string){
-    const post: Post = {title: title, content: content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    let url = 'http://localhost:3000/api/posts';
+    const post: Post = {id: "1",title: title, content: content};
+    this.http.post<{message: string}>(url,post)
+      .subscribe( (response) => {
+        console.log(response.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      })
   }
-
 }
